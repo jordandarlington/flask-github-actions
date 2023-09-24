32c0cff766aa4eaeaa0b737268ea29f910b6b625
Jordan Darlington <jordan@europa.local>
2023-09-24 14:48:58 +0100

    initial commit

---
+++ app/main.py
@@ -0,0 +1,14 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return 'Welcome!'
+
+@app.route('/<string>')
+def reverse_string(string):
+    return "".join(reversed(string))
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080)
