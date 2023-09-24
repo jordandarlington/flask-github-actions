3d67405957c0fb0ec63c715b5c9a44a01e95e9ef
Jordan Darlington <jordandarlington@duck.com>
2023-09-24 15:03:22 +0100

    adds example unit tests

---
+++ app/test_main.py
@@ -0,0 +1,12 @@
+from main import home, reverse_string
+import unittest
+
+class TestMain(unittest.TestCase):
+    def test_home(self):
+        self.assertEquals("Welcome!", home())
+
+    def test_reverse_string(self):
+        self.assertEquals("gnirts tset", reverse_string("test"))
+
+if __name__ == "__main__":
+    unittest.main()
